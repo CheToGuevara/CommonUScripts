bccf79ca934140073a07ed8e7c5b2de8b9f97325
CheToGuevara <aronano@gmail.com>
2020-04-13 11:21:29 +0200

    feat: GifRender

---
+++ Functionality/GifRender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class GifRender : MonoBehaviour
+{
+
+    public Image ImgToRender = null;
+    public Sprite[] frames;
+    public float framesPerSecond = 10;
+
+    void Update()
+    {
+
+        int index = Mathf.RoundToInt(Time.time * framesPerSecond);
+        index = index % frames.Length;
+        ImgToRender.sprite = frames[index]; 
+
+    // Update is called once per frame
+    
+        
+    }
+}
